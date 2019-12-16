671ea6200f5f7224f0e5980e843404790ff7a4ca
Elise Deitrick <edeitrick@codio.com>
2019-12-16 23:41:53 +0000

    thru for loops reading

---
+++ .guides/turtle.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd code/loops/
+$1 $2 Turtle.java
+$3 $4 $5 $6
---
+++ code/loops/TurtleLoopImages.java
@@ -0,0 +1,9 @@
+public class TurtleLoopImages
+{
+    public static void main(String[] args) 
+    {
+
+
+      
+    }
+}
