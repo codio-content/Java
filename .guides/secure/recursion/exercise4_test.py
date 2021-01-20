cce56dd315366ab8c922a280b45299f33c6c1a2a
Patrick Ester <patricke@codio.com>
2021-01-20 04:18:46 +0000

    Add auto-grader for exercise 4

--- .guides/secure/recursion/exercise4_test.py
+++ .guides/secure/recursion/exercise4_test.py
@@ -1,23 +1,42 @@
-import os, sys, random, re, io, subprocess
+import os, sys, requests, random, re, io, subprocess
+sys.path.append('/usr/share/codio/assessments')
+from lib.grade import send_partial_v2, FORMAT_V2_MD, FORMAT_V2_HTML, FORMAT_V2_TXT
 #change for correct language
-from java_grader_methods import *
+import java_grader_methods as jgm
 
 ## uses the above method to grade programs
 def main():
-    path = 'code/recursion'
+    path = 'code/recursion/'
     file = 'Exercise4.java'
-    destination = 'recursion/' + file
     student_code = os.path.join(path, file)
-    copy_code_file(student_code, destination)
+    points = 100
+    feedback = ''
+    
+    if not(jgm.check_output(path, file, 'calvin', 'nivlac', False)):
+      feedback += 'Test case failed: "calvin" \n'
+      points -= 20
+      
+    if not(jgm.check_output(path, file, 'hobbes', 'sebboh', False)):
+      feedback += 'Test case failed: "hobbes" \n'
+      points -= 20
+      
+    if not(jgm.check_output(path, file, 'jerry', 'yrrej', False)):
+      feedback += 'Test case failed: "jerry" \n'
+      points -= 20
+      
+    if not(jgm.check_output(path, file, 'wormwood', 'doowmrow', False)):
+      feedback += 'Test case failed: "wormwood" \n'
+      points -= 20
     
     # loop through line-by-line to check for more than keywords
-    with open('.guides/secure/recursion/' + file, 'r') as student_code:
-      code_without_comments = strip_comments(student_code.read())
-      
-      if not(check_code_for_recursive_call(code_without_comments, 'reverseString', 'public static int reverseString')):
-        sys.exit(1)
-      else:
-        sys.exit(0)
+    with open(student_code, 'r') as java_code:
+      if not(jgm.check_code_for_recursive_call(java_code.readlines(), 'reverseString', 'public static String reverseString')):
+        feedback += '<code>reverseString</code> method is not recursive \n'
+        points -= 20
+
+    feedback += '<h2>On this question you earned {}% out of 100%</h2>'.format(points)
+    res = send_partial_v2(points, feedback, FORMAT_V2_HTML)
+    exit(0 if res else 1)
 
 if __name__ == "__main__":
     main()
---
+++ code/recursion/Exercise4.java
@@ -0,0 +1,14 @@
+public class Exercise4 {
+  public static void main(String[] args) {
+
+    //DO NOT alter code below this line
+    System.out.println(reverseString(args[0]));
+    //DO NOT alter code above this line
+  }
+
+  //add method definitions below this line
+  
+  
+  
+  //add method definitions above this line
+}
