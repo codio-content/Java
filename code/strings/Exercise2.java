e82b2ef4116a69d5122f11652245164905b0c7c2
Anh Le <ale@codio.com>
2021-02-03 01:37:38 +0000

    changed variables in student files

--- code/strings/Exercise2.java
+++ code/strings/Exercise2.java
@@ -1,7 +1,7 @@
 public class Exercise2 {
   public static void main(String args[]) {
     
-    String my_string = args[0];
+    String myString = args[0];
     
     //add code below this line
     
