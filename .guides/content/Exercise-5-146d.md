62d69dab4493ae01baf2a3bfc9503e18cff6db55
Patrick Ester <patricke@codio.com>
2021-03-02 20:14:43 +0000

    Change Exercise 5 to a JUnit test

--- .guides/content/Exercise-5-146d.md
+++ .guides/content/Exercise-5-146d.md
@@ -3,7 +3,7 @@
 ## Recursion Exercise 5
 
 **Problem**
-Write a recursive method called `getMax` that takes an array of integers as a parameter. Return the largest integer in the array. **Do not** alter the code on lines 7 to 11; this is needed for the evaluation.
+Write a recursive method called `getMax` that takes an array of integers as a parameter. Return the largest integer in the array.
 
 <details>
   <summary><strong>Hint</strong></summary>
@@ -15,7 +15,6 @@ Write a recursive method called `getMax` that takes an array of integers as a pa
 * If the `getMax` method is called with the array `{11, 22, 3, 41, 15}` then it would return `41`
 
 [Code Visualizer](open_tutor code/recursion/Exercise5.java)
-{Try with 1, 2, 3, 4, 5}(sh .guides/bg.sh javac code/recursion/Exercise5.java java -cp code/recursion/ Exercise5 1 2 3 4 5)
-{Try with 11, 22, 3, 41, 15}(sh .guides/bg.sh javac code/recursion/Exercise5.java java -cp code/recursion/ Exercise5 11 22 3 41 15)
+{Try it}(sh .guides/bg.sh javac code/recursion/Exercise5.java java -cp code/recursion/ Exercise5 1)
 
 {Check It!|assessment}(test-2073587953)
--- .guides/secure/recursion/Exercise5Tester.java
+++ .guides/secure/recursion/Exercise5Tester.java
@@ -3,40 +3,44 @@ import org.junit.Test;
 import org.junit.Before;
 import java.io.*;
 
-public class Exercise4Tester {
+public class Exercise5Tester {
   
-  Exercise4 studentCode;
+  Exercise5 studentCode;
   
   @Before
   public void setup() {
-    studentCode = new Exercise4();
+    studentCode = new Exercise5();
   }
   
   @Test
   public void check1() {
-    assertEquals(studentCode.reverseString("calvin"), "nivlac");
+    int[] testList = {10, 20, 30, 40};
+    assertEquals(studentCode.getMax(testList), 40);
   }
   
   @Test
   public void check2() {
-    assertEquals(studentCode.reverseString("hobbes"), "sebboh");
+    int[] testList = {130, -5, 15, 30};
+    assertEquals(studentCode.getMax(testList), 130);
   }
   
   @Test
   public void check3() {
-    assertEquals(studentCode.reverseString("jerry"), "yrrej");
+    int[] testList = {40, 320, 80, 160};
+    assertEquals(studentCode.getMax(testList), 320);
   }
   
   @Test
   public void check4() {
-    assertEquals(studentCode.reverseString("wormwood"), "doowmrow");
+    int[] testList = {15, 10, 20, 5};
+    assertEquals(studentCode.getMax(testList), 20);
   }
   
   @Test 
   public void checkForRecursion() {
     
-    String path = "code/recursion/Exercise4.java";
-    String methodName = "bunnyEars";
+    String path = "code/recursion/Exercise5.java";
+    String methodName = "getMax";
     int methodCount = 0;
     String code = "";
     
