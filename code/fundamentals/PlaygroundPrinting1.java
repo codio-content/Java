e5194cdd0ef6abe60cde54ea838bfc6486a41fd9
Anh Le <ale@codio.com>
2021-08-09 13:35:47 +0000

    publishing intern qa edits

--- code/fundamentals/PlaygroundPrinting1.java
+++ code/fundamentals/PlaygroundPrinting1.java
@@ -2,7 +2,7 @@ public class PlaygroundPrinting1 {
   public static void main(String args[]) {
     
     //add code below this line
-    
+    System.out.print(A);
     
     //add code above this line
   }
